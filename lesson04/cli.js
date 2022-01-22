d677a93e281bbed386bc7f9aece13764f07a30a7
petrashko <petrashko@tpu.ru>
2022-01-22 21:13:12 +0700

    Lesson04

--- lesson04/cli.js
+++ lesson04/cli.js
@@ -1,22 +1,74 @@
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
+const readline = require('readline');
+//
+const inquirer = require('inquirer');
+const yargs = require('yargs');
 
-const fileName = 'log.txt';
-const filePath = path.join(__dirname, fileName);
+const options = yargs
+    .usage('Usage: -s <string to search>')
+    .option('s', {
+        alias: 'search',
+        describe: 'String to search',
+        type: 'string',
+        demandOption: true
+    }).argv;
 
-console.log(`START!!!${os.EOL}${os.EOL}`);
+//console.log(options.search);
 
-if (fs.existsSync(filePath)) {
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            throw err;
+const searchInFile = (filePath, search) => {
+    const readLines = readline.createInterface({
+        input: fs.createReadStream(filePath, 'utf8'),
+        crlfDelay: Infinity
+    });
+    
+    readLines.on('line', (str) => {
+        if (str.includes(search)) {
+            console.log(`${search} найдено в => ${str}`);
         }
-
-        console.log( Buffer.from(data).toString() );
+    });
+    
+    readLines.on('close', () => {
         console.log(`${os.EOL}${os.EOL}END!!!`);
     });
+    /*
+    readLines.on('error', (err) => {
+        console.log(err);
+    });
+    */
 }
-else {
-    console.log('File not found');
+
+const isFile = (fileName) => fs.lstatSync(fileName).isFile();
+
+const chooseFile = (curDir) => {
+    const fileList = fs.readdirSync(curDir);
+    
+    inquirer.prompt([
+        {
+            name: 'fileName',
+            type: 'list', // input, number, confirm, list, checkbox, password
+            message: 'Выберите файл для чтения',
+            choices: fileList,
+        }
+    ]).then((result) => {
+        //console.log(result);
+        let fileOrDir = '';
+        if (curDir === `.${path.sep}`) {
+            fileOrDir = path.join(__dirname, result.fileName);
+        }
+        else {
+            fileOrDir = path.join(curDir, result.fileName);
+        }
+        //console.log(fileOrDir);
+
+        if (isFile(fileOrDir) === true) {
+            searchInFile(fileOrDir, options.search);
+        }
+        else {
+            chooseFile(fileOrDir);
+        }
+    });
 }
+
+chooseFile(`.${path.sep}`);
